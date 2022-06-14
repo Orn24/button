d65bb9095896a11303697f08ee2d7ecb5f4aef34
Orn <58498965+Orn24@users.noreply.github.com>
2022-06-14 10:53:43 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Hello!")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+basic.forever(function () {
+	
+})
