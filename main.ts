e7082bf0bd79869aba9cfccad929b42019b73fe4
Orn <58498965+Orn24@users.noreply.github.com>
2022-06-19 22:20:08 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showString("Hello!")
+    basic.showIcon(IconNames.Heart)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
 })
 basic.forever(function () {
 	
